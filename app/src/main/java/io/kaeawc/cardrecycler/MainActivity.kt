84527bfc019b724a021ff0c6bc7967e4c1118a7d
Jason Pearson <jason.d.pearson@gmail.com>
2017-02-15 16:06:54 -0500

    added presenter

--- app/src/main/java/io/kaeawc/cardrecycler/MainActivity.kt
+++ app/src/main/java/io/kaeawc/cardrecycler/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val presenter = DiscoverPresenter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        presenter.onCreate(this)
     }
 }
